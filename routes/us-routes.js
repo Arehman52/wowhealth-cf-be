8158532683f0de95961745135b3d12b698badd7a
Abdur Rehman <abdurrehman418@gmail.com>
2022-08-01 13:52:52 +0500

    pagination and query search updated

--- routes/us-routes.js
+++ routes/us-routes.js
@@ -23,11 +23,12 @@ export const GenericResponseObject = {
       "totalNumberOfPages": 0,
       "totalNumberOfRecords": 0,
       "numberOfRecordsPerPage": 10,
-      "order": "DESC",
+      "order": "ASC",
       "paginationEnabled": true,
       "offSet": 0
   }
 }
+
 
 
 
@@ -125,8 +126,17 @@ router.get("/getUsers/getPeeps", (req, res, next) => {
 
 
 router.get("/getAppointmentsHistory", (req, res, next) => {
-  
-  
+  // SELECT * FROM tbl LIMIT 5,10;  # Retrieve rows 6-15
+  var offset = (req.query.pageNumber - 1) * req.query.numberOfRecordsPerPage;
+  var sql = '';
+  if(req.query.q != ''){
+    sql = 'SELECT * FROM `tbl_appointment_history` WHERE ser_appointment_history_id LIKE "%'+req.query.q+'%"';
+  }else{
+    sql = 'select * from `tbl_appointment_history` LIMIT '+offset+','+(req.query.numberOfRecordsPerPage * req.query.pageNumber);
+  }
+  console.log(sql);
+  console.log(req.query);
+
   var con = mysql.createConnection({
       host: "35.210.208.100",
       user: "devuser",
@@ -137,27 +147,30 @@ router.get("/getAppointmentsHistory", (req, res, next) => {
     });
   
   con.connect(function(err) {
+    GenericResponseObject.status.tokenExpired = false;
+    GenericResponseObject.status.contextID = 'not available';
     if (err)
     {
-      let errorResp = {};
-          errorResp.message = "ERROR: unknown reason!";
-          errorResp.exceptionMessage = err;
-          res.status(301).send(errorResp);
+      GenericResponseObject.status.message = "ERROR: unknown reason!";
+      GenericResponseObject.status.result = err;
+      res.status(301).send(GenericResponseObject);
     }
     console.log("Connected!");
   });
-  
-  
-  con.query(
-    'select * from `tbl_appointment_history`',
-    function(err, results, fields) {
+
+  con.query( sql, function(err, results, fields) {
+    // results = [...results, ...results, ..  .results]
       if(err) {
         GenericResponseObject.status.message = "ERROR: Error or incorrect SQL query or API";
         GenericResponseObject.status.result = err;
         res.status(301).send(GenericResponseObject);
         }
-      // var keys = [];
-      // fields.forEach((item)=>{ keys.push(item['name'])});
+        GenericResponseObject.pagination.totalNumberOfRecords = results.length;
+        GenericResponseObject.pagination.totalNumberOfPages = Math.ceil(results.length / GenericResponseObject.pagination.numberOfRecordsPerPage);
+        
+        
+
+
       let mappingConfigs = {
         "ser_appointment_history_id": "appointmentHistoryId",
         "txt_operation_name" : "operationName",
@@ -167,23 +180,12 @@ router.get("/getAppointmentsHistory", (req, res, next) => {
         "fk_user_id" : "userId"
       }
       let mappedDataa = mapper(results,mappingConfigs);
-      try {
-        results.forEach((value, index, array)=>{
-          let obj = {
-            "appointmentHistoryId" : value['ser_appointment_history_id'],
-            "operationName" : value['txt_operation_name'],
-            "dte_date" : value['dte_date'],
-            "appointmentId" : value['fk_appointment_id'],
-            "details" : value['txt_details'],
-            "userId" : value['fk_user_id']
-          };
-          console.log(mappedDataa);
-          GenericResponseObject.data.push(mappedDataa);
-        });
-        
+      try {        
+        GenericResponseObject.data = [...mappedDataa];
         res.status(200).send(GenericResponseObject);
       } catch (error) {
         GenericResponseObject.status.message = "ERROR: Error Occured Due to an Exception";
+        GenericResponseObject.status.result = error;
         res.status(301).send(GenericResponseObject);
       }
     }
@@ -207,106 +209,34 @@ export function mapper(response, mappingConfigs) {
   return mapped;
 }
 
-  
-
-
-// router.get("/getAppointmentsHistory", (req, res, next) => {
-//   console.log('req.params');
-//   console.log(req.params+'');
-//   var con = mysql.createConnection({
-//       host: "35.210.208.100",
-//       user: "devuser",
-//       password: "devuser@339",
-//       port:3306,
-//       database: 'wowpk_dev',
-//       uri:"jdbc:mysql://35.210.208.100:3306/wowpkdev",
-//     });
-  
-//   con.connect(function(err) {
-//     if (err)
-//     {
-//       let errorResp = {};
-//           errorResp.message = "ERROR: unknown reason!";
-//           errorResp.exceptionMessage = err;
-//           res.status(301).send(errorResp);
-//     }
-//     console.log("Connected!");
-//   });
-  
-  
-//   con.query(
-//     'select * from `tbl_appointment_history`',
-//     function(err, results, fields) {
-//       if(err) {
-//         GenericResponseObject.status.message = "ERROR: Error or incorrect SQL query or API";
-//         GenericResponseObject.status.result = err;
-//         res.status(301).send(GenericResponseObject);
-//         }
-//       // var keys = [];
-//       // fields.forEach((item)=>{ keys.push(item['name'])});
-//       try {
-//         results.forEach((value, index, array)=>{
-//           let obj = {
-//             "appointmentHistoryId" : value['ser_appointment_history_id'],
-//             "operationName" : value['txt_operation_name'],
-//             "dte_date" : value['dte_date'],
-//             "appointmentId" : value['fk_appointment_id'],
-//             "details" : value['txt_details'],
-//             "userId" : value['fk_user_id']
-//           };
-//           GenericResponseObject.data.push(obj);
-//         });
-        
-//         res.status(200).send(GenericResponseObject);
-//       } catch (error) {
-//         GenericResponseObject.status.message = "ERROR: Error Occured Due to an Exception";
-//         res.status(301).send(GenericResponseObject);
-//       }
-//     }
-//   );
-
-// });
-  
 
 
 
 router.get("/", (req, res, next) => {
-  res.status(200).send("You have visitied the US route.")
-
-  // var con = mysql.createConnection({
-  //   host: "35.210.208.100",
-  //   user: "devuser",
-  //   password: "devuser@339",
-  //   port:3306,
-  //   database: 'wowpk_dev',
-  //   uri:"jdbc:mysql://35.210.208.100:3306/wowpkdev",
-  // });
-
-  // con.connect(function(err) {
-  //   if (err) throw err;
-  //   console.log("Connected!");
-  // });
-
-
-  // con.query('select * from `tbl_appointment_history`',
-  // // con.query("SELECT COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS where table_schema = 'wowpkdev' AND TABLE_NAME='tbl_appointment_history'",
-  //   function(err, results, fields) {
-  //     if(err) console.log(err);
-  //     var resp = {}, keys = [];
-  //     fields.forEach((x)=>{ keys.push(x['name'])});
-  //     // Object.assign(resp,{"FIELDS" : keys});
-  //     Object.assign(resp,{"FIELDS": keys});  
-  //     Object.assign(resp,{"RESULTS": results});  
-  //     try {
-  //       res.send(resp);
-  //     } catch (error) {
-  //       console.log(error)
-  //       res.send(error+'');
-
-  //     }
-  //   }
-  // );
-
+  res.status(200).send("You have visitied the US route.");
 });
 
 export default router;
+
+
+
+
+
+
+  // var keys = [];
+  // fields.forEach((item)=>{ keys.push(item['name'])});
+
+
+
+
+
+    // results.forEach((value, index, array)=>{
+      // let obj = {
+      //   "appointmentHistoryId" : value['ser_appointment_history_id'],
+      //   "operationName" : value['txt_operation_name'],
+      //   "dte_date" : value['dte_date'],
+      //   "appointmentId" : value['fk_appointment_id'],
+      //   "details" : value['txt_details'],
+      //   "userId" : value['fk_user_id']
+      // };
+    // });
